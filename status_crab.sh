c4a6b530805e16eb722de5355dc873d2fae7a7eb
IzaakWN <iwn_@hotmail.com>
2019-10-25 16:56:09 +0200

    first commit

---
+++ status_crab.sh
@@ -0,0 +1,10 @@
+#! /bin/bash
+# Author: Izaak Neutelings (October, 2019)
+function peval { echo ">>> $@"; eval "$@"; }
+
+TASKS="$@"
+
+for task in $TASKS; do
+  #echo ">>> $task"
+  peval "crab status -d $task"
+done
