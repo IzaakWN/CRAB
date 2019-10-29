2726c24a746de06cd1825ee83640070ffec31114
IzaakWN <iwn_@hotmail.com>
2019-10-29 15:51:26 +0100

    fix options

--- crab.sh
+++ crab.sh
@@ -2,19 +2,26 @@
 # Author: Izaak Neutelings (October, 2019)
 function peval { echo -e ">>> \e[1m$@\e[0m"; eval "$@"; }
 
+# ENSURE ARGUMENTS
 if [[ $# -lt 2 ]]; then
   echo ">>> Not enough arguments! Please use 'crab.sh <command> <task directories>'."
   exit 1
 fi
 
+# PARSE ARGUMENTS
 CMD="$1"
-TASKS="${@:2}"
+OPTS=""
+TASKS="" 
+for arg in "${@:2}"; do
+  [ "${arg:0:2}" = '--' ] && OPTS+="$arg " || TASKS+="$arg "
+done
 
+# EXECUTE
 if [ "$CMD" = "rm" ]; then
   peval "rm -rf $TASKS"
 else
   for task in $TASKS; do
     #echo ">>> crab $CMD -d $task"
-    peval "crab $CMD -d $task"
+    peval "crab ${CMD} ${OPTS}-d ${task}"
   done
 fi
