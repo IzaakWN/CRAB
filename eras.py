defd267d6f32bb89dcdd44eff65ba64398977155
IzaakWN <iwn_@hotmail.com>
2020-06-04 11:18:37 +0200

    add more eras

--- eras.py
+++ eras.py
@@ -11,13 +11,26 @@ globaltags['mc']['miniAOD'] = {
   2017:      '94X_mc2017_realistic_v17', #'94X_mc2017_realistic_v14',
   2018:      '102X_upgrade2018_realistic_v20', #'102X_upgrade2018_realistic_v15',
 }
-globaltags['mc']['nanoAOD'] = {
+globaltags['mc']['NanoAODv5'] = {
   'default': 'auto:phase1_2017_realistic',
   2016:      '102X_mcRun2_asymptotic_v7',
   2017:      '102X_mc2017_realistic_v7', #'94X_mc2017_realistic_v14',
   2018:      '102X_upgrade2018_realistic_v19', #'102X_upgrade2018_realistic_v20', #'102X_upgrade2018_realistic_v15',
 }
-globaltags['data']['nanoAOD'] = {
+globaltags['mc']['NanoAODv6'] = {
+  'default': 'auto:phase1_2017_realistic',
+  2016:      '102X_mcRun2_asymptotic_v7',
+  2017:      '102X_mc2017_realistic_v7', #'94X_mc2017_realistic_v14',
+  2018:      '102X_upgrade2018_realistic_v20', #'102X_upgrade2018_realistic_v20', #'102X_upgrade2018_realistic_v15',
+}
+globaltags['data']['NanoAODv5'] = {
+  'default': 'auto:phase1_2017_realistic',
+  2016:      '102X_dataRun2_v11',
+  2017:      '102X_dataRun2_v11',
+  2018:      '102X_dataRun2_v12',
+  '2018D':   '102X_dataRun2_Prompt_v15',
+}
+globaltags['data']['NanoAODv6'] = {
   'default': 'auto:phase1_2017_realistic',
   2016:      '102X_dataRun2_v11',
   2017:      '102X_dataRun2_v11',
@@ -27,7 +40,12 @@ globaltags['data']['nanoAOD'] = {
 
 from Configuration.StandardSequences.Eras import eras as Eras
 eras = { }
-eras['nanoAOD'] = {
+eras['NanoAODv5'] = {
+  2016: Eras.run2_nanoAOD_94X2016,
+  2017: Eras.run2_nanoAOD_94XMiniAODv2,
+  2018: Eras.run2_nanoAOD_102Xv1,
+}
+eras['NanoAODv6'] = {
   2016: Eras.run2_nanoAOD_94X2016,
   2017: Eras.run2_nanoAOD_94XMiniAODv2,
   2018: Eras.run2_nanoAOD_102Xv1,
